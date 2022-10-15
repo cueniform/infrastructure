ee5757801c8137612fc051f11b57d3df5e7f86cb
Jonathan Matthews <github@hello.jonathanmatthews.com>
2022-10-15 15:28:18 +0300

    Add www and apex A records; output zone NS

---
+++ cue/outputs.tf.cue
@@ -0,0 +1,3 @@
+output: cueniform_com_NS: {
+	value: "${resource.aws_route53_zone.cueniform_com.name_servers}"
+}
--- hcl/main.tf
+++ hcl/main.tf
@@ -1,3 +1,25 @@
 resource "aws_route53_zone" "cueniform_com" {
   name = "cueniform.com"
 }
+
+resource "aws_route53_record" "www_cueniform_com_A" {
+  zone_id = aws_route53_zone.cueniform_com.zone_id
+  name    = "www.cueniform.com"
+  type    = "A"
+  ttl     = 3600
+  records = [
+    "66.111.4.53",
+    "66.111.4.54",
+  ]
+}
+
+resource "aws_route53_record" "cueniform_com_A" {
+  zone_id = aws_route53_zone.cueniform_com.zone_id
+  name    = "cueniform.com"
+  type    = "A"
+  ttl     = 3600
+  records = [
+    "66.111.4.53",
+    "66.111.4.54",
+  ]
+}
